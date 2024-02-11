d531bfe0cfbf5cc5ce9c0fad4272ae21e6371e4c
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-11 01:49:17 +0100

    Add central part of Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,7 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Col, Button } from "react-bootstrap";
 import Logo from "../assets/Logo.svg";
+import Cartella from "../assets/Cartella.jpg";
+import Calendar from "../assets/Calendar.jpg";
 
 export const Homepage = () => {
   return (
@@ -20,6 +22,38 @@ export const Homepage = () => {
         <Col lg={6} md={4}>
           <Button className="butt1">Log-in</Button>
           <Button className="butt2">Sign-Up</Button>
+        </Col>
+      </div>
+      <div className="centerpart">
+        <Col lg={6}>
+          <img src={Cartella} alt="Cartella" className="Folder" />
+          <div className="textcalendar">
+            <h1>Prenota & Ricorda !</h1>
+            <p>
+              Archivia qui tutti i tuoi dati in modo tale da tenere sempre
+              d'occhio <br /> i tuoi documenti delle tue visite passate orem
+              ipsum <br />
+              dolor sit amet, consectetur adipisici elit, sed eiusmod tempor{" "}
+              <br />
+              incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam.
+            </p>
+          </div>
+        </Col>
+        <Col lg={6}>
+          <div className="textarchivio">
+            <h1>Archivia!</h1>
+            <p>
+              Archivia qui tutti i tuoi dati in modo tale da tenere sempre
+              d'occhio <br /> i tuoi documenti delle tue visite passate orem
+              ipsum <br />
+              dolor sit amet, consectetur adipisici elit, sed eiusmod tempor{" "}
+              <br />
+              incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam.
+            </p>
+          </div>
+          <img src={Calendar} alt="Calendario" className="Calendar" />
         </Col>
       </div>
     </section>
