d1e4beac10d2caf8afc38f95a9cdad9785da5b3f
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-19 19:28:15 +0100

    Modify Redux2.0

--- src/redux/reducers/homeReducer.js
+++ src/redux/reducers/homeReducer.js
@@ -3,12 +3,18 @@ import {
   GET_ARCHIVE,
   GET_USER_LOGGED,
   LOGOUT_USER,
+  GET_PRENOTAZIONI_DA_SALVARE,
+  GET_PRENOTAZIONI_DA_MODIFICARE,
+  GET_PRENOTAZIONI_DA_ELIMINARE,
 } from "../actions";
 
 const initialState = {
   prenotazioni: [],
   archive: [],
   user: null,
+  prenotazioneSave: null,
+  prenotazioneEdit: null,
+  prenotazioneDelete: null,
 };
 
 const homeReducer = (state = initialState, action) => {
@@ -32,6 +38,21 @@ const homeReducer = (state = initialState, action) => {
       return {
         ...state,
         user: null,
+      };
+    case GET_PRENOTAZIONI_DA_SALVARE:
+      return {
+        ...state,
+        prenotazioneSave: action.payload,
+      };
+    case GET_PRENOTAZIONI_DA_MODIFICARE:
+      return {
+        ...state,
+        prenotazioneEdit: action.payload,
+      };
+    case GET_PRENOTAZIONI_DA_ELIMINARE:
+      return {
+        ...state,
+        prenotazioneDelete: action.payload,
       };
     default:
       return state;
