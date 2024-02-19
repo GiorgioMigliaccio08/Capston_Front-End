ec608f044f80df78ade3810192b23afcf58102bc
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-19 19:27:46 +0100

    Modify Redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,6 +2,22 @@ export const GET_BOOKING = "GET_BOOKING";
 export const GET_ARCHIVE = " GET_ARCHIVE";
 export const GET_USER_LOGGED = "GET_USER_LOGGED";
 export const LOGOUT_USER = "LOGOUT_USER";
+export const GET_PRENOTAZIONI_DA_SALVARE = "salva";
+export const GET_PRENOTAZIONI_DA_MODIFICARE = "modifica";
+export const GET_PRENOTAZIONI_DA_ELIMINARE = "elimina";
+
+export const getBookingSave = (booking) => ({
+  type: GET_PRENOTAZIONI_DA_SALVARE,
+  payload: booking,
+});
+export const getBookingModify = (booking) => ({
+  type: GET_PRENOTAZIONI_DA_MODIFICARE,
+  payload: booking,
+});
+export const getBookingDelete = (booking) => ({
+  type: GET_PRENOTAZIONI_DA_ELIMINARE,
+  payload: booking,
+});
 
 export const getBooking = (url) => {
   const token = localStorage.getItem("token");
