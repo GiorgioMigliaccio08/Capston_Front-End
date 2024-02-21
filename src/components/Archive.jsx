057d198b9c8ea85c1a3e78619db9991adfebc967
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-02-21 17:43:21 +0100

    Add Archive.css

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -60,52 +60,52 @@ const Archive = () => {
         <div className="linkprenotazione">
           <Nav className="navlinks">
             <Link to="/Homepage">
-              <Nav.Link href="home" className="navlink2">
+              <button className="buttunaccess1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  color=" #withe"
+                  width="25"
+                  height="25"
+                  color=" #00aeb5"
                   fill="currentColor"
-                  class="bi bi-house-door-fill"
+                  className="bi bi-house-door-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
                 </svg>
-                Home
-              </Nav.Link>
+                <p className="textaccess">Home</p>
+              </button>
             </Link>
             <Link to="/Prenotazioni">
-              <Nav.Link href="booking" className="navlink2">
+              <button className="buttunaccess1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  color=" withe"
+                  width="25"
+                  height="25"
+                  color=" #00aeb5"
                   fill="currentColor"
-                  class="bi bi-calendar-plus-fill"
+                  className="bi bi-calendar-plus-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M8.5 8.5V10H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V11H6a.5.5 0 0 1 0-1h1.5V8.5a.5.5 0 0 1 1 0" />
                 </svg>
-                Prenotazioni
-              </Nav.Link>
+                <p className="textaccess">Prenotazioni</p>
+              </button>
             </Link>
             <Link to="/Archivio">
-              <Nav.Link href="archive" className="navlink2">
+              <button className="buttunaccess1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  color=" withe"
+                  width="25"
+                  height="25"
+                  color=" #00aeb5"
                   fill="currentColor"
-                  class="bi bi-archive-fill"
+                  className="bi bi-archive-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z" />
                 </svg>
-                Archivio
-              </Nav.Link>
+                <p className="textaccess">Archivio</p>
+              </button>
             </Link>
           </Nav>
         </div>
