fac186b88b01f143588efdb86987161c41e1293a
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-02 19:37:00 +0100

    Add modify to Archive

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -4,6 +4,9 @@ import { Nav, Card } from "react-bootstrap";
 import "../assets/style/Archive.css";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
+import { IoHome } from "react-icons/io5";
+import { FaCalendarPlus } from "react-icons/fa";
+import { RiArchive2Fill } from "react-icons/ri";
 
 const Archive = () => {
   const [showForm, setShowForm] = useState(false);
@@ -124,53 +127,41 @@ const Archive = () => {
         </div>
         <div className="linkprenotazionearchive">
           <Nav className="navlinks">
-            <Link to="/Homepage">
-              <button className="buttunaccess1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  color=" #00aeb5"
-                  fill="currentColor"
-                  className="bi bi-house-door-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
-                </svg>
-                <p className="textaccess">Home</p>
-              </button>
+            <Link to="/Homepage" style={{ textDecoration: "none" }}>
+              <div className="links">
+                <IoHome
+                  style={{
+                    fontSize: "35px",
+                    marginRight: "10px",
+                    color: "#0089DC",
+                  }}
+                />
+                Home
+              </div>
             </Link>
-            <Link to="/Prenotazioni">
-              <button className="buttunaccess1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  color=" #00aeb5"
-                  fill="currentColor"
-                  className="bi bi-calendar-plus-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M8.5 8.5V10H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V11H6a.5.5 0 0 1 0-1h1.5V8.5a.5.5 0 0 1 1 0" />
-                </svg>
-                <p className="textaccess">Prenotazioni</p>
-              </button>
+            <Link to="/Prenotazioni" style={{ textDecoration: "none" }}>
+              <div className="links">
+                <FaCalendarPlus
+                  style={{
+                    fontSize: "35px",
+                    marginRight: "10px",
+                    color: "#0089DC",
+                  }}
+                />
+                Prenotazioni
+              </div>
             </Link>
-            <Link to="/Archivio">
-              <button className="buttunaccess1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  color=" #00aeb5"
-                  fill="currentColor"
-                  className="bi bi-archive-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1M.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8z" />
-                </svg>
-                <p className="textaccess">Archivio</p>
-              </button>
+            <Link to="/Archivio" style={{ textDecoration: "none" }}>
+              <div className="links">
+                <RiArchive2Fill
+                  style={{
+                    fontSize: "35px",
+                    marginRight: "8px",
+                    color: "#0089DC",
+                  }}
+                />{" "}
+                Archivio
+              </div>
             </Link>
           </Nav>
         </div>
